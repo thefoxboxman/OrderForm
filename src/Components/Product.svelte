e8e6e529cf7e942ab77017b862b0e3559601fa7b
John <john@John-New-MacPro.local>
2020-05-06 16:42:47 +1000

    Improving layout of Products and Navbar

--- src/Components/Product.svelte
+++ src/Components/Product.svelte
@@ -18,57 +18,97 @@
 
 <style>
   .item {
-    /* display: flex;
+    display: flex;
     justify-content: space-between;
-    align-items: center; */
+    align-items: center;
     text-align: center;
     padding-left: 0.5em;
-    border: 2px solid black;
+    /* border: 2px solid black; */
   }
   input {
     width: 30px;
     color: green;
     text-align: right;
+    margin-bottom: 0;
+    padding: 0;
+    /* border: 0 solid black; */
   }
 
   #buyButton {
     margin-top: 0.5em;
+    margin-left: 1em;
     background-color: green;
     color: whitesmoke;
     cursor: pointer;
+    max-width: 5rem;
+  }
+  .title {
+    display: flex;
+
+    /* border: 2px solid black; */
+    flex-grow: 1;
+    justify-content: right;
+    align-items: center;
+    margin-right: 1em;
+    max-width: 15rem;
+  }
+  .order {
+    display: flex;
+
+    /* border: 2px solid black; */
+    flex-grow: 1;
+    color: green;
+    align-items: center;
+    min-width: 5rem;
+    max-width: 15rem;
   }
   .emoji {
     display: inline-block;
     font-size: 1.5rem;
     margin: 0.1em;
   }
+
+  .item p {
+    margin: 0 0;
+  }
 </style>
+
+<!-- ############################################## -->
 
 <div class="product">
 
-  {#if product.unit == 'kg'}
-    <div class="item">
+  <!-- {#if product.unit == 'kg'} -->
+  <div class="item">
+
+    <div class="title">
       <p class="emoji">{product.emoji}</p>
-      <div>
-        <p>{product.name}</p>
-        <p>
-          ${product.price}/{product.unit} Enter quantity required:
-          <input bind:value={product.quantity} />
-          {product.unit} Total: ${(product.quantity * product.price).toFixed(2)}
-        </p>
-      </div>
-      <button id="buyButton" class="btn" on:click={buyProduct}>
-        Add to Cart
-      </button>
+      <p>{product.name} ${product.price}/{product.unit}</p>
     </div>
-  {:else}
+    <div class="order">
+      <p>Order:</p>
+      <p>
+        <input bind:value={product.quantity} />
+        {product.unit}
+      </p>
+    </div>
+    <!-- <div class="total">
+        Total: ${(product.quantity * product.price).toFixed(2)}
+      </div> -->
+    <button id="buyButton" class="btn" on:click={buyProduct}>
+      Add to Cart
+    </button>
+  </div>
+  <!-- {:else}
     <div class="item">
       <p class="emoji">{product.emoji}</p>
       <div>
         <p>{product.name}</p>
+        <p>${product.price}/{product.unit}</p>
         <p>
-          ${product.price}/{product.unit} Enter quantity required:
+          Enter quantity required:
           <input bind:value={product.quantity} />
+        </p>
+        <p>
           {product.unit} Total: ${(product.quantity * product.price).toFixed(2)}
         </p>
       </div>
@@ -77,7 +117,7 @@
       </button>
     </div>
   {/if}
-
+-->
 </div>
 
 <!-- <button id="buyButton" class="btn" on:click={buyProduct}>Add to Cart</button> -->
--- src/GlobalComps/NavBar.svelte
+++ src/GlobalComps/NavBar.svelte
@@ -3,16 +3,35 @@
 </script>
 
 <style>
-  nav {
+  .container {
+    /* border: 2px solid green; */
     display: flex;
     justify-content: space-between;
-    border: 2px solid green;
-    padding: 1rem;
+    align-items: center;
+  }
+
+  logo {
+  }
+  img {
+    width: 3em;
+  }
+  nav {
+    display: flex;
+    justify-content: space-around;
+    font-size: 1rem;
+    /* border: 2px solid green; */
+    /* padding: 1rem; */
+    flex-grow: 1;
   }
 </style>
 
-<nav>
-  <a href="/">Home</a>
-  <a href="contact-us">Contact Us</a>
-  Items in cart 0
-</nav>
+<div class="container">
+  <div class="logo">
+    <img src="../logo.png" alt="cons logo" />
+  </div>
+  <nav class="nav">
+    <a href="/">Home</a>
+    <a href="contact-us">Contact Us</a>
+    Items in cart 0
+  </nav>
+</div>
